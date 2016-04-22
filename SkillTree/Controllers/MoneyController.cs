9cebfdc512f01fdb60eae43b6738b80e8b95e2f5
Chao-Shiun <e4366.tw@gmail.com>
2016-04-22 21:51:55 +0800

    移除MoneyController第一週內容

--- SkillTree/Controllers/MoneyController.cs
+++ SkillTree/Controllers/MoneyController.cs
@@ -17,21 +17,7 @@ namespace SkillTree.Controllers
 
         public ActionResult List()
         {
-            List<Money> MoneyList = new List<Money>();
-            Random rand = new Random();
-
-            for (int i = 0; i < 10; i++)
-            {
-                MoneyList.Add(
-                    new Money()
-                    {
-                        Amount = rand.Next() % 1000000,
-                        Category = (i % 3 == 0 ? "提款" : "存入"),
-                        Date = DateTime.Now.AddDays(rand.Next() % 30)
-                    }
-                );
-            }
-            return View(MoneyList.OrderBy(p => p.Category).ToList());
+            return View();
         }
     }
 }
